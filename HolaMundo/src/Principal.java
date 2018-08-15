1cf92e7b298c2b3bd15e285caf6d74ca0daf3488
Juan Dario Rodas Marin <jdrodas@hotmail.com>
2018-08-15 09:01:49 -0500

    Actualización del proyecto HolaMundo

--- HolaMundo/src/Principal.java
+++ HolaMundo/src/Principal.java
@@ -12,7 +12,36 @@ Visualizar información en la consola, en forma de cadena de caracteres.
 public class Principal {
 
 	public static void main(String[] args) {
+
+        //Comenzamos con la forma más básica de colocar información en consola.
+        //Utilizamos el método println de System.out
+        //Escribe una cadena de caracteres, delimitada por las comillas dobles (")
 		System.out.println("Hola Mundo!");
+
+        //Esto mismo se puede lograr con el método print() más el caracter especial \n
+        // \n corresponde al salto de línea - tecla Intro/Retorno
+        System.out.print("Hola Mundo!\n");
+        
+        //Aqui declaramos una variable tipo string
+        String unaFrase;
+
+        //Aqui le asignamos el valor inicial
+        unaFrase = "Esta es una frase";
+
+        //Aqui declaramos y asignamos en una sola sentencia
+        String otraFrase = "Esta es otra frase";
+
+        //aqui visualizamos las frases, una por línea
+        System.out.println(unaFrase);
+        System.out.println(otraFrase);
+
+        //Una invocación a print con solo el caracter especial \n 
+        //genera un salto de línea
+        System.out.print("\n");
+        
+        //Aqui podemos visualizar concatenando las frases con el operador + y
+        //el caracter especial \n para el salto de línea
+        System.out.println(unaFrase + "\n" + otraFrase + "\n");
 
 	}
 
--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Proyectos del curso de Fundamentos de Programación en Java
 Inventario de proyectos en el repositorio:
 
 ## Hola Mundo
-Visualizar información en la consola, en forma de cadena de caracteres.
+- Visualizar información en la consola, en forma de cadena de caracteres.
+- Declarar variables tipo string y visualizar su contenido en la consola.
+- Demostrar el funcionamiento de los métodos println() y print() de System.out.
