7f4a125369adb73c6e4d1c2150728ee42781029a
Juan Dario Rodas Marin <jdrodas@hotmail.com>
2018-08-15 08:36:00 -0500

    Agregado el proyecto HolaMundo

---
+++ HolaMundo/src/Principal.java
@@ -0,0 +1,19 @@
+/*
+Programa:       HolaMundo
+Contacto:       Juan Dario Rodas - jdrodas@hotmail.com
+
+Propósito:
+----------
+Demostrar el funcionamiento básico del Entorno Integrado de Desarrollo (IDE), realizando
+las actividades de edición de código, compilación, depuración y ejecución de la aplicación.
+Visualizar información en la consola, en forma de cadena de caracteres.
+
+*/
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo!");
+
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Java_funprog
-Proyectos del curso de Fundamentos de ProgramaciÃ³n en Java
+Proyectos del curso de Fundamentos de Programación en Java
+
+Inventario de proyectos en el repositorio:
+
+## Hola Mundo
+Visualizar información en la consola, en forma de cadena de caracteres.
